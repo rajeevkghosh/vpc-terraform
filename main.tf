6001f291e1b7a8d95b645fe551ff05024c29f921
rajeevkghosh <54052253+rajeevkghosh@users.noreply.github.com>
2021-09-21 13:04:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
-provider “google” {
+provider "google" {
 
-  version = “3.5.0”
+  version = "3.5.0"
   access_token = var.access_token
-  project = “tokyo-nomad-323506”
-  region  = “us-central1”
-  zone    = “us-central1-c”
+  project = "tokyo-nomad-323506"
+  region  = "us-central1"
+  zone    = "us-central1-c"
   
 }
 
-resource “google_compute_network” “vpc_network” {
+resource "google_compute_network" "vpc_network" {
 
-  name = “terraform-network”
+  name = "terraform-network"
 }
 
-resource “google_compute_subnetwork” “public-subnetwork” {
+resource "google_compute_subnetwork" "public-subnetwork" {
 
-  name          = “terraform-subnetwork”
-  ip_cidr_range = “10.2.0.0/16”
-  region        = “us-central1”
+  name          = "terraform-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-central1"
   network       = google_compute_network.vpc_network.name
   }
